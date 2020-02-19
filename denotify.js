6e3f8371c546b138d93e1e2c6aa4191e338f3973
Pamplemousse <xav.maso@gmail.com>
2020-02-19 03:43:23 -0700

    Denotify LinkedIn with JS

--- denotify.js
+++ denotify.js
@@ -1,1 +1,17 @@
-document.body.style.border = "5px solid green";
+(function() {
+  let domains =
+    [ 'linkedin'
+    ]
+
+  let domains_regexp = new RegExp(domains.join('|'))
+  let current_domain = domains_regexp.exec(document.domain)[0]
+
+  if (current_domain === undefined) {
+    return
+  }
+
+  let function_name = 'denotify_' + current_domain
+  let denotify_function = this[function_name]
+
+  denotify_function()
+} ())
